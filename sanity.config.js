a39c172688166a7fefabc3aaf840a4d05e2c9383
amphreyomosh <amphreyomosh2001@gmail.com>
2024-12-20 16:54:01 +0300

    Updated sanity.config.js

--- sanity.config.js
+++ sanity.config.js
@@ -2,7 +2,7 @@ import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {whyGladCard} from './schemaTypes/whyGladCard'
-import herosection from './schemaTypes/herosection'
+import{ heroSection} from './schemaTypes/heroSection'
 import services from './schemaTypes/services'
 
 export default defineConfig({
@@ -15,6 +15,6 @@ export default defineConfig({
   plugins: [structureTool(), visionTool()],
 
   schema: {
-    types: [whyGladCard, herosection, services],
+    types: [whyGladCard, heroSection, services],
   },
 })
